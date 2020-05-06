ef7112cbda756b33a76be9da597358d619b7041d
fanwenjie <fanwenjie@mysise.org>
2020-05-07 00:34:34 +0800

    spring cloud gateway  alibaba  sentinel 动态流控 熔断实现

--- mysise-gateway/src/main/java/org/mysise/gateway/MysiseGatewayApplication.java
+++ mysise-gateway/src/main/java/org/mysise/gateway/MysiseGatewayApplication.java
@@ -2,7 +2,9 @@ package org.mysise.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MysiseGatewayApplication {
 
