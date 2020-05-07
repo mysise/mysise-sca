cd5ff5421fd64169138f226d3c840e62f323c5b9
fanwenjie <fanwenjie@mysise.org>
2020-05-07 16:59:11 +0800

    favicon.ico 设置

--- mysise-service/mysise-common-service/src/main/java/org/mysise/common/controller/BaseController.java
+++ mysise-service/mysise-common-service/src/main/java/org/mysise/common/controller/BaseController.java
@@ -1,12 +1,5 @@
 package org.mysise.common.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * <p>
  *  公共处理controller
@@ -15,17 +8,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author FanWenJie
  * @since 2020/5/7 0:45
  */
-@RestController
-public class BaseController implements ErrorController {
+public class BaseController {
 
 
-    @Autowired
-    HttpServletRequest request;
-
-    @Override
-    @GetMapping("/error")
-    public String getErrorPath() {
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        return "没找到相应请求";
-    }
 }
