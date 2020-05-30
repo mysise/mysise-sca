686941dd46b7af06a2faf80a44df783ecaa6d6c7
fanwenjie <fanwenjie@mysise.org>
2020-05-30 11:40:30 +0800

    [modify]: 选用nacos 本地部署

--- mysise-service/mysise-demo1/src/main/java/org/mysise/demo1/rest/Demo1Api.java
+++ mysise-service/mysise-demo1/src/main/java/org/mysise/demo1/rest/Demo1Api.java
@@ -4,10 +4,10 @@ import org.mysise.common.model.Result;
 import org.mysise.demo.feign.IDemoClient;
 import org.mysise.demo1.service.Demo1Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +15,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 
 @RestController
+@RequestMapping("/api/demo1")
 public class Demo1Api {
 
     AtomicInteger id = new AtomicInteger(1);
